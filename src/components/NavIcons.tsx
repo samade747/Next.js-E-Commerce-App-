0b87a62b23c571800cc377fc5d977785c8f89e4a
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-16 11:06:09 +0500

    1106am 16 07 2024

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { useState } from 'react'
+import CartModal from './CartModal'
 
 const NavIcons = () => {
 
@@ -19,25 +20,48 @@ const NavIcons = () => {
 
     const handleProfile = () => {
         if(isLoggedIn){
-            setIsProfileOpen(prev => !prev)
+            router.push("/login")
         }
+        setIsProfileOpen((prev) => !prev)
     }
 
 
   return (
     <div className='flex items-center gap-4 xl:gap-6'>
-        <Image src="/profile.png" alt="heart" width={24} height={24} className='cursor-pointer' />
-
-        {isProfileOpen && <div className=''>
+        <Image src="/profile.png"
+               alt="heart" 
+               width={24}
+               height={24}
+               className='cursor-pointer'
+               onClick={handleProfile}
+        />
+        {isProfileOpen &&
+         <div className='absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20'>
             <Link href="/">Profile</Link>
-            <div className='mt-2 cursor-pointer'>
-                Logout
-            </div>
-            
+            <div className='mt-2 cursor-pointer'>Logout</div>            
         </div>
         }
-        <Image src="/notification.png" alt="heart" width={24} height={24} className='cursor-pointer' />
-        <Image src="/cart.png" alt="heart" width={24} height={24} className='cursor-pointer' />
+        <Image src="/notification.png"
+               alt="heart"
+               width={24}
+               height={24} 
+               className='cursor-pointer'
+         />
+        <div className='relative cursor-pointer'>
+        <Image src="/cart.png"
+               alt="heart"
+               width={24}
+               height={24}
+               className='cursor-pointer'
+               onClick={() => setIsCartOpen((prev) => !prev)}
+               />
+               <div className='absolute -top-4 right-4 w-6 h-6 bg-samad rodded-full text-white text-sm flex items-center justify-center justify-items-center'>2
+
+
+               </div>
+        </div>
+        {isCartOpen && <CartModal />}
+
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,15 +22,23 @@ const Navbar = () => {
             <div className="hidden md:flex items-center justify-between gap-8 h-full">
             
                {/* LEFT */}
-                <div className="w-1/3">
+                <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
                 <Link href="/" className="flex items-center gap-3">
                     <Image src="/logo.png" alt="logo" width={24} height={24} />
                 </Link>
+                <div className="hidden xl:flex gap-4">
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Shop</Link>
+                    <Link href="/">Deals</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+
+                </div>
                 </div>
 
 
                {/* RIGHT */}
-               <div className="w-2/3 flex items-center justify-between gap-8">
+               <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
                     <SearchBar />
                     <NavIcons />
                </div>
