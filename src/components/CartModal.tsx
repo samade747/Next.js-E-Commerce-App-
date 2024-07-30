5578eeef5e5b4a9f9143c78ac3bfaf70daef24db
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-31 04:07:20 +0500

    Update CartModal.tsx

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,9 +1,25 @@
 "use client"
-import React from 'react'
+
+import Image from 'next/image'
 
 const CartModal = () => {
+
+  const cartItmes = true
+
   return (
-    <div>CartModal</div>
+    <div className='absolute p-4 rounded-lg bg-white bg-white top-12 right-0 flex flex-col gap-4'>
+    {!cartItmes ? (
+        <div>
+          <p>Cart is empty</p>
+        </div>
+    ): (
+      <div className: "flex flex-col" >
+          <Image src={"/logo.png"} alt="logo" width={24} height={24}>
+      </div>
+    )}
+
+
+    </div>
   )
 }
 
