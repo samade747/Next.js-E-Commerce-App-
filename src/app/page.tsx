d4d2a3e7da3e2d1cf997fedea32b8fcb4d3ad6f7
samade747 <samad.e747@gmail.com>
2024-08-08 12:45:59 +0500

    1245 am

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,26 @@ import Slider from "@/components/Slider"
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="">
       <Slider />
-
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
           <h1 className="/text">Featured Products</h1>
           <ProductList />
       </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+          <h1 className="/text">Categories</h1>
+          <ProductList />
+      </div>
+
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+          <h1 className="/text">New Products</h1>
+          <ProductList />
+      </div>
+
+
+
 
     </div>
   )
