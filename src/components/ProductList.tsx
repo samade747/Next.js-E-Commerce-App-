4c278feae72cc968b700f09453528af45ff08493
samade747 <samad.e747@gmail.com>
2024-08-08 12:13:08 +0500

    Update ProductList.tsx

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -6,7 +6,7 @@ const ProductList = () => {
 
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-           <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45% ] lg:[22%]'>
+           <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45% ] lg:w-[22%]'>
             <div className='relative w-full h-80'>
                 <Image
                     src="https://images.pexels.com/photos/2916814/pexels-photo-2916814.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -31,11 +31,44 @@ const ProductList = () => {
                 </div>            
 
                  <div className='text-sm text-gray-500' >My description  </div>
-                <button className='rounded-2xl ring-1 ring-black px-4 py-3 px-4 text-xs hover:hover'>Add to cart</button>
+                <button className='rounded-2xl ring-1 ring-black px-4 py-3 px-4 text-xs hover:bg-black hover:text-white'>Add to cart</button>
 
 
                
-            </Link>                       
+            </Link>       
+
+
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45% ] lg:[22%]'>
+            <div className='relative w-full h-80'>
+                <Image
+                    src="https://images.pexels.com/photos/2916814/pexels-photo-2916814.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt=""
+                    fill 
+                    sizes='25vw'
+                    className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'  
+                />                            
+                
+                <Image 
+                    src="https://images.pexels.com/photos/2916814/pexels-photo-2916814.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt=""
+                    fill 
+                    sizes='25vw'
+                    className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'  
+                />
+                </div>
+
+                <div className='flex justify-between'>
+                    <span className='font-semi-bold'>PRODUCT NAME</span>
+                    <span className='p-1 bg-gray-200 rounded-sm'>$49</span>                                    
+                </div>            
+
+                 <div className='text-sm text-gray-500' >My description  </div>
+                <button className='rounded-2xl ring-1 ring-black px-4 py-3 px-4 text-xs hover:bg-black hover:text-white'>Add to cart</button>
+
+
+               
+            </Link>       
+
         </div>
     )
 }   
