dad7aca2cba75e294561df2b8107153046e6737f
samade747 <samad.e747@gmail.com>
2024-08-08 13:20:08 +0500

    120

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProductList from "@/components/ProductList"
 import Slider from "@/components/Slider"
+import CategoryList from "@/components/CategoryList"
 
 const HomePage = () => {
   return (
@@ -12,7 +13,7 @@ const HomePage = () => {
 
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
           <h1 className="/text">Categories</h1>
-          <ProductList />
+          <CategoryList />
       </div>
 
 
--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,6 +1,26 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+
 const CategoryList = () => {
     return (
-        <div>CategoryList</div>
+        <div className="px-4 overflow-x-scroll">
+            <div className="flex gap-4 md:gap-8">
+                <Link href="/list?cat=test">
+                    <div className='relative bg-size-100 w-full h-90'>
+                        <Image 
+                            src="https://images.pexels.com/photos/2916814/pexels-photo-2916814.jpeg?auto=compress&cs=tinysrgb&w=600"
+                            fill
+                            sizes='25vw'
+                            className='object-cover rounded-md'
+                            
+
+                        />
+                    </div>
+                    <h1 className='mt-8 font-light text-cl tracking-wide'>Category Name</h1>
+                </Link>
+            </div>
+        </div>
     )
 }
 
